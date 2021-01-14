e09033626c0092ae06b15da8a9ff00de7cf1d112
linhhonblade <meomaimaimeo@gmail.com>
2021-01-14 09:19:37 +0700

    change theme color to aht brand color

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'AHT Theme Backend',
+    'summary': 'AHT Theme Backend',
+    'sequence': 15,
+    'description': "AHT Theme Backend",
+    'depends': ['web'],
+    'data': [
+        'views/assets.xml',
+    ],
+}
